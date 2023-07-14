cd3269ccf971da62028ad6484332c815532aad77
huakui <40330631+jtzhpf@users.noreply.github.com>
2023-07-14 18:00:38 +0800

    ADD libfdt-dev

--- add-apt-env.sh
+++ add-apt-env.sh
@@ -1,2 +1,2 @@
 apt update
-apt install gcc cmake g++ build-essential pkg-config zlib1g-dev libglib2.0-dev meson libpixman-1-dev ninja-build -y
+apt install gcc cmake g++ build-essential pkg-config zlib1g-dev libglib2.0-dev meson libpixman-1-dev ninja-build libfdt-dev -y
